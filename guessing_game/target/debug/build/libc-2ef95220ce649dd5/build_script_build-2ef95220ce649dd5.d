0d615357cc2ab09b01646e0e5d54f266b5541b6a
Jan Lauber <jan.lauber@protonmail.ch>
2022-02-22 16:34:22 +0100

    remove guessing game
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- guessing_game/target/debug/build/libc-2ef95220ce649dd5/build_script_build-2ef95220ce649dd5.d
+++
@@ -1,5 +0,0 @@
-/Users/janlauber/code/github.com/janlauber/learning-rust/guessing_game/target/debug/build/libc-2ef95220ce649dd5/build_script_build-2ef95220ce649dd5: /Users/janlauber/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.119/build.rs
-
-/Users/janlauber/code/github.com/janlauber/learning-rust/guessing_game/target/debug/build/libc-2ef95220ce649dd5/build_script_build-2ef95220ce649dd5.d: /Users/janlauber/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.119/build.rs
-
-/Users/janlauber/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.119/build.rs:
